397e5f1538bbbcc1a4bb8cb038b91f44ea22052e
MRITUNJAY <mritunjaykumartiwari1@gmail.com>
2023-07-31 16:46:51 +0530

    basics of datatype

---
+++ 01_Frontend development/03_Javascript/01_Core JS/02_Data types/index.js
@@ -0,0 +1,77 @@
+// JavaScript has 8 Datatypes
+// 1. String
+// 2. Number
+// 3. Bigint
+// 4. Boolean
+// 5. Undefined
+// 6. Null
+// 7. Symbol
+// 8. Object
+
+
+// String datatype
+
+// let str="Mritunjay"
+// console.log(str);
+// console.log(typeof str);
+
+// single quote inside double quotes
+// let f_name="'Mritunjay kumar tiwari'"
+
+// double quote inside double quote
+// let f_name='"Mritunjay kumar tiwari"'
+// console.log(f_name);
+
+
+// Number datatype
+
+// let age=22;
+// console.log(age);
+// console.log(typeof age);
+
+// let num=2.3;
+// console.log(num);
+// console.log(typeof num);
+
+// Exponential Notation
+// let num=1e5;
+// let num=1e-5;
+// console.log(num);
+// console.log(typeof num);
+
+
+// BitgInt
+// let num=BigInt(1234)
+// let num=1234n
+// console.log(num);
+// console.log(typeof num);
+
+// Boolean :true/false
+// let x=5
+// let y=10
+// console.log(x>y);
+// console.log(typeof(x>y));
+// console.log(x<y);
+// console.log(typeof(x<y));
+
+// undefined:It means value is not provided to the variable
+// let x;
+// console.log(x);
+// console.log(typeof x);
+
+// null:It means object does not contain any reference
+// let obj={}
+// console.log(obj);
+// console.log(typeof obj);
+// let obj=null
+// console.log(obj);
+// console.log(typeof obj);
+
+
+// Object
+let obj={
+    name:"Mritunjay",
+    place:"Ara"
+}
+console.log(obj);
+console.log(typeof obj);
